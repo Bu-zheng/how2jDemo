f8ec8092694562cb904ac5662471b1d030d98fc1
Bu-zheng <5777411@qq.com>
2019-09-29 11:38:53 +0800

    github上传测试3

--- src/example/com/bz/Test.java
+++ src/example/com/bz/Test.java
@@ -51,5 +51,7 @@ public class Test {
         System.out.println(objects.size());
         System.out.println(objects.size());
         System.out.println(objects.size());
+        System.out.println(objects.size());
+        System.out.println(objects.size());
     }
 }
